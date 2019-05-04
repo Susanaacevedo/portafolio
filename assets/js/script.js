efedc3fa4db48af2d89095c031ebd846114ca5e0
Susana <sacevedo.jimenez@gmail.com>
2019-05-04 01:09:29 -0400

    Añadiendo js, carrusel y toolpip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 // funcion todo lo dentro de la funcion se ejecutara una vez cargada la pagina
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
+  });
+
+  $('[data-toggle="popover"]').popover();
+
+});
